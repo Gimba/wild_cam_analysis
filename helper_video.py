c76985fc3df2bf2c1230eeb57bbe384f3c5851f9
Gimba <martin.rosellen@fu-berlin.de>
2022-10-23 13:58:06 +0200

    better folder name for extracted images

--- helper_video.py
+++ helper_video.py
@@ -4,12 +4,11 @@ from pathlib import Path
 
 def extract_image_from_video(video_file, frame_interval):
     pth = Path(video_file)
-
     video_data = cv2.VideoCapture(video_file)
 
     try:
         # creating a folder named data
-        folder = 'extracted_images_' + pth.stem
+        folder = os.path.join(pth.parent,pth.stem)
         if not os.path.exists(folder):
             os.makedirs(folder)
 
