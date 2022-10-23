d2bb23f3f03ca9c669359f16ffb1f0fb25d67959
Gimba <martin.rosellen@fu-berlin.de>
2022-10-23 14:03:58 +0200

    function for extracting of frames complete

--- helper_video.py
+++ helper_video.py
@@ -2,16 +2,44 @@ import cv2
 import os
 from pathlib import Path
 
-def extract_image_from_video(video_file, frame_interval):
+
+def extract_image_from_video(video_file, frame_interval=100):
     pth = Path(video_file)
     video_data = cv2.VideoCapture(video_file)
 
     try:
         # creating a folder named data
-        folder = os.path.join(pth.parent,pth.stem)
+        folder = os.path.join(pth.parent, pth.stem)
         if not os.path.exists(folder):
             os.makedirs(folder)
 
     # if not created then raise error
     except OSError:
         print('Error: Creating directory of data')
+
+    # frame counter
+    current_frame = -1
+
+    while (True):
+        current_frame += 1
+        # reading from frame
+        ret, frame = video_data.read()
+        print(current_frame % frame_interval)
+        if ret:
+            if current_frame % frame_interval == 0:
+                # if video is still left continue creating images
+                name = os.path.join(folder, 'frame_' + str(current_frame) + '.jpg')
+                print('Creating...' + name)
+
+                # writing the extracted images
+                cv2.imwrite(name, frame)
+
+                # increasing counter so that it will
+                # show how many frames are created
+
+        else:
+            break
+
+    # Release all space and windows once done
+    video_data.release()
+    cv2.destroyAllWindows()
